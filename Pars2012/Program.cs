abaef37179874baa57d0a9919d57c87a38b1cc72
Juhász Zoltán <juhaszzoltanmate@yahoo.com>
2023-01-13 15:11:30 +0100

    Add project files.

---
+++ Pars2012/Program.cs
@@ -0,0 +1,24 @@
+﻿using Pars2012;
+
+List<Versenyzo> versenyzok = new();
+
+using StreamReader sr = new(@"..\..\..\RES\Selejtezo2012.txt");
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+    versenyzok.Add(new Versenyzo(sr.ReadLine()!));
+
+Console.WriteLine($"5. feladat: Versenyzők száma a selejtezőben: {versenyzok.Count} fő");
+
+int atj = versenyzok.Count(x => x.Dobasok.Contains(-2f));
+Console.WriteLine($"6. feladat: 78,00 méter feletti eredménnyel továbbjutott: {atj} fő");
+
+var lje = versenyzok.OrderBy(x => x.Eredmeny).Last();
+Console.WriteLine("9. feladat: A selejtező nyertese:\n" +
+    $"\tNév: {lje.Nev}\n" +
+    $"\tCsoport: {lje.Csoport}\n" +
+    $"\tNemzet: {lje.Nemzet}\n" +
+    $"\tNemzet kód: {lje.Kod}\n" +
+    $"\tSorozat: {lje.Sorozat}\n" +
+    $"\tEredmény: {lje.Eredmeny}");
+
+Console.ReadKey(true);
---
+++ Pars2012/Versenyzo.cs
@@ -0,0 +1,39 @@
+﻿namespace Pars2012
+{
+    internal class Versenyzo
+    {
+        public string Nev { get; set; }
+        public char Csoport { get; set; }
+        private string NemzetEsKod { get; set; }
+        public string Sorozat { get; private set; }
+        public float[] Dobasok { get; set; }
+        public float Eredmeny => Dobasok.Max();
+
+        public string Nemzet =>
+            NemzetEsKod.Substring(0, NemzetEsKod.LastIndexOf(' '));
+
+        public string Kod =>
+            NemzetEsKod.Substring(NemzetEsKod.LastIndexOf(' ') + 1).Trim('(', ')');
+
+        public Versenyzo(string sor)
+        {
+            string[] splts = sor.Split(';');
+
+            Nev = splts[0];
+            Csoport = char.Parse(splts[1]);
+            NemzetEsKod = splts[2];
+            Dobasok = new float[3];
+            for (int i = 0; i < Dobasok.Length; i++)
+            {
+                Dobasok[i] = splts[i + 3] switch
+                {
+                    "X" => -1f,
+                    "-" => -2f,
+                    _ => float.Parse(splts[i + 3])
+                };
+                Sorozat += $"{splts[i + 3]};";
+            }
+            Sorozat = Sorozat!.Substring(0, Sorozat.Length - 1);
+        }
+    }
+}
